6377b9a42d69a14f09f56d1ae40c1bd067bd43d2
WellyKid <48344775+WellyKid@users.noreply.github.com>
2023-09-01 20:54:06 +0000

    add dynamic data and job items to home page along with API endpoint

--- app.py
+++ app.py
@@ -1,11 +1,32 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
+
+JOBS = [{
+  'id': 1,
+  'title': 'Data Analyst',
+  'location': 'Los angeles',
+  'salary': 'USD $100,000'
+}, {
+  'id': 2,
+  'title': 'Control Systems Engineer',
+  'location': 'Seattle',
+  'salary': 'USD $140,000'
+}, {
+  'id': 3,
+  'title': 'Software Engineer',
+  'location': 'Ketchikan',
+}]
 
 
 @app.route("/")
 def hello_world():
-  return render_template("home.html")
+  return render_template("home.html", jobs=JOBS)
+
+
+@app.route("/api/jobs")
+def list_jobs():
+  return jsonify(JOBS)
 
 
 if __name__ == "__main__":
